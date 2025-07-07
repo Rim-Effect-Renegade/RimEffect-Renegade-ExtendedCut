07d0ff37a1ff726a888f761d4eed1987b5410a0c
Neronix17 <39186138+neronix17@users.noreply.github.com>
2025-07-07 19:28:19 +0100

    1.6 Ready

--- 1.6/Source/RimEffectExtendedCut/CompPowerOutDoorLamp.cs
+++ 1.6/Source/RimEffectExtendedCut/CompPowerOutDoorLamp.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
-using VanillaFurnitureExpanded;
+using VEF.Buildings;
 using Verse;
 
 namespace RimEffectExtendedCut
